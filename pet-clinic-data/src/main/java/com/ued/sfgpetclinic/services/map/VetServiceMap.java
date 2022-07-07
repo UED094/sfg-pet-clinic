d97d96fd85cd1325ab7a61de6761a54f49c11f79
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-06 20:23:10 -0700

    Implemented map based services. closes #6

---
+++ pet-clinic-data/src/main/java/com/ued/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,35 @@
+package com.ued.sfgpetclinic.services.map;
+
+import com.ued.sfgpetclinic.model.Vet;
+import com.ued.sfgpetclinic.services.CrudService;
+import com.ued.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
