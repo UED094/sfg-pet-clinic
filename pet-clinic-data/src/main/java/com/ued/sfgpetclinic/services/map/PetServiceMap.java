869b4ccf755fd9f078eb742792ce43c23c1c29bf
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-07 16:55:47 -0700

    Add bootstrap data on startup. closes #14

--- pet-clinic-data/src/main/java/com/ued/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/ued/sfgpetclinic/services/map/PetServiceMap.java
@@ -2,10 +2,11 @@ package com.ued.sfgpetclinic.services.map;
 
 import com.ued.sfgpetclinic.model.Pet;
 import com.ued.sfgpetclinic.services.CrudService;
+import com.ued.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
