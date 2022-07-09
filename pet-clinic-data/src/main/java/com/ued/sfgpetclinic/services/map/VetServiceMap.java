130b22c23422db3084a4fd4bbdaaee47bac1d108
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-08 17:21:30 -0700

    Added Specialities and added to vets.  Closes #35

--- pet-clinic-data/src/main/java/com/ued/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/ued/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,7 +1,9 @@
 package com.ued.sfgpetclinic.services.map;
 
+import com.ued.sfgpetclinic.model.Speciality;
 import com.ued.sfgpetclinic.model.Vet;
 import com.ued.sfgpetclinic.services.CrudService;
+import com.ued.sfgpetclinic.services.SpecialityService;
 import com.ued.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -27,6 +35,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if (object.getSpecialities().size() > 0) {
+            object.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality =specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
+
         return super.save(object);
     }
 
