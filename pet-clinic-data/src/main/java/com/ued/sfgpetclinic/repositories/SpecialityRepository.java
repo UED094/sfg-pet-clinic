687ad7009888c41bf8beebbb31ce66c23fee95bb
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-13 16:04:09 -0700

    Fixed the specialty repository bug. Closes #48

--- pet-clinic-data/src/main/java/com/ued/sfgpetclinic/repositories/SpecialityRepository.java
+++ pet-clinic-data/src/main/java/com/ued/sfgpetclinic/repositories/SpecialityRepository.java
@@ -1,6 +1,7 @@
 package com.ued.sfgpetclinic.repositories;
 
+import com.ued.sfgpetclinic.model.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
-public interface SpecialityRepository extends CrudRepository<SpecialityRepository, Long> {
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
 }
