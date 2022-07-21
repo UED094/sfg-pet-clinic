61b1bee5702e7cc563f0f758b6462284a9a20578
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-21 15:06:48 -0700

    Find owner by like fixed. Closes #80

--- pet-clinic-web/src/main/java/com/ued/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ued/sfgpetclinic/controllers/OwnerController.java
@@ -34,7 +34,7 @@ public class OwnerController {
             owner.setLastName("");
         }
 
-        List<Owner> results = ownerService.findAllByLastNameLike(owner.getLastName());
+        List<Owner> results = ownerService.findAllByLastNameLike("%" + owner.getLastName() + "%");
 
         if (results.isEmpty()) {
             result.rejectValue("lastName","notFound","not found");
@@ -43,7 +43,7 @@ public class OwnerController {
             owner = results.get(0);
             return "redirect:/owners/" + owner.getId();
         } else {
-            model.addAttribute("selections", results);
+            model.addAttribute("listOwners", results);
             return "owners/ownersList";
         }
     }
