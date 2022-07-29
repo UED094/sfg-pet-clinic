1c274cedd7e6bd92243c92c35d7480a660e530a1
Ugur Dogan <ugur.dogan@resmed.com>
2022-07-28 17:56:41 -0700

    Fixed update pet issue. Closes #101

--- pet-clinic-web/src/main/java/com/ued/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/ued/sfgpetclinic/controllers/PetController.java
@@ -63,8 +63,8 @@ public class PetController {
             result.rejectValue("name","duplicate","already exists");
         }
 
-        pet.setOwner(owner);
         owner.getPets().add(pet);
+        pet.setOwner(owner);
         if (result.hasErrors()) {
             model.put("pet",pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
@@ -88,7 +88,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
-            owner.getPets().add(pet);
+            pet.setOwner(owner);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
         }
